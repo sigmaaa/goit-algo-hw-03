485d19837107eae6f5fdae14769ff3bad20f4da3
Denys Almazov <almazovdenys@gmail.com>
2024-10-17 18:40:32 +0300

    Update first_task.py

--- first_task.py
+++ first_task.py
@@ -2,7 +2,8 @@ import sys
 import os
 import shutil
 
-def recursive_file_search(source_path,dest_path):
+
+def recursive_file_search(source_path, dest_path):
     for dirpath, dirnames, filenames in os.walk(source_path):
         for filename in filenames:
             file_path = os.path.join(dirpath, filename)
@@ -12,18 +13,25 @@ def recursive_file_search(source_path,dest_path):
                 new_dir = os.path.join(dest_path, extension)
                 os.makedirs(new_dir, exist_ok=True)
                 dest_file_path = os.path.join(new_dir, filename)
-                shutil.copy(file_path, dest_file_path)
+                try:
+                    shutil.copy(file_path, dest_file_path)
+                except PermissionError:
+                    print("Permission denied to copy f{file_path}:")
+
+                print(f"copied {file_path} to {dest_file_path}")
         for dir in dirnames:
-            recursive_file_search(os.path.join(dirpath, dir),dest_path)
+            recursive_file_search(os.path.join(dirpath, dir), dest_path)
+
 
 if __name__ == "__main__":
-    if len(sys.argv) < 3:
+    if len(sys.argv) < 2:
         print("Usage: python script.py <source_path> <dest_path>")
         sys.exit(1)
-
     source_path = sys.argv[1]
-    dest_path = sys.argv[2]
+    if len(sys.argv) == 2:
+        dest_path = "dist"
+    else:
+        dest_path = sys.argv[2]
 
     print(f"Destination path: {dest_path}")
     recursive_file_search(source_path, dest_path)
-
